653680e5419a5a34fd2c6c3ea16d33778f3ab472
weichen <georgew8638@hotmail.com>
2016-02-14 22:57:06 +0800

    logo

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 <meta charset="UTF-8" />
 <title>Internatioanl Companions for Learning</title>
+<link rel="shortcut icon" href="imgages/favicon.ico">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <style type="text/css">
 * {
